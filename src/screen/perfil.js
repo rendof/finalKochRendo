e47bfa6f531f18ce83a3e5fcf54003f73dd33f3d
Francisco Rendo <dm,n f;kxdsf.me>
2021-11-23 23:17:03 -0300

    pagina de perfil funcionando

--- src/screen/perfil.js
+++ src/screen/perfil.js
@@ -1,27 +1,52 @@
 import React,{Component} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 import { auth, db } from '../firebase/config';
+import Fotos from '../components/paginafotos';
 
 
 class Perfil extends Component{
     constructor(props){
       super(props)
       this.state={
-        posts:[],
+        post:[],
         deletePost: ''
       }
     }
-
+    componentDidMount(){
+      
+      db.collection("posts").where("user", "==", auth.currentUser.email).onSnapshot((docs)=>{
+        
+        let posts = []
+        docs.forEach((doc)=>{
+          
+            posts.push({
+                id: doc.id,
+                data: doc.data()
+            })
+        })
+        this.setState({
+          
+            post: posts
+        })
+    })
+    }
+      
     render(){
   return (
     <View style={styles.container}>
       <Text>Pagina del Perfil!!</Text>
       <Text>Email del usuario: {auth.currentUser.email}</Text>
-      <Text>Fecha de Cracion: {}</Text>
-      <Text>Ultima Conexion: {}</Text>
+      <Text>Fecha de Creacion: {auth.currentUser.metadata.creationTime}</Text>
+      <Text>Ultima Conexion: {auth.currentUser.metadata.lastSignInTime}</Text>
       <TouchableOpacity  onPress={()=> this.props.logout()}>
                 <Text>Desloguearse</Text>
             </TouchableOpacity>
+            <Text style={styles.titulo} >Publicaciones</Text>
+      <FlatList  
+                    data={this.state.post} // tiene que tener la info que le vamso a dar que le mandamos de mas arriba los posteos
+                    keyExtractor={(data)=> data.id}
+                    renderItem={(item)=>( <Fotos style={styles.posteo} data={item}/> )}  >
+                </FlatList>
 
       
     </View>
@@ -36,6 +61,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontFamily: 'arial',
+    fontSize: 30,
+  }
 });
 
 export default Perfil
